cb7cec7cab9989da1265d152bc5e70652dbfc1dd
Peter Westman <pwestman@gmail.com>
2020-06-19 17:22:12 +0200

    Remove names from users and implement auth pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  # Others available :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  # Others available :recoverable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   has_many :orders
-
-  validates :first_name, length: { minimum: 3 , maximum: 40 }
 end
